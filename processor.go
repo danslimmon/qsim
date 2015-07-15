69c0a4752c1c74785dd4b70aa3a39af28ca1a13c
Dan Slimmon <dan.slimmon@gmail.com>
2015-07-15 21:42:43 +0000

    Beefed up the test for Processor.Start

--- processor.go
+++ processor.go
@@ -1,5 +1,9 @@
 package qsim
 
+import (
+	"errors"
+)
+
 // A Processor is the piece of the queueing system that processes jobs.
 type Processor struct {
 	CurrentJob        *Job
@@ -12,14 +16,14 @@ type Processor struct {
 // For example, if you wanted half of the jobs to take 10 ticks to
 // process, and the other half to take 20 ticks, you could do this:
 //
-//	ptg := func(j *Job) int {
-//		if rand.Float32() < 0.5 {
-//			return 10
-//		} else {
-//			return 20
-//		}
-//	}
-//	p.SetProcTimeGenerator(ptg)
+//    ptg := func(j *Job) int {
+//        if rand.Float32() < 0.5 {
+//            return 10
+//        } else {
+//            return 20
+//        }
+//    }
+//    p.SetProcTimeGenerator(ptg)
 func (p *Processor) SetProcTimeGenerator(ptg func(j *Job) int) {
 	p.procTimeGenerator = ptg
 }
@@ -27,9 +31,23 @@ func (p *Processor) SetProcTimeGenerator(ptg func(j *Job) int) {
 // Start begins processing a given job.
 //
 // The return value is the amount of time it'll take to process the job.
-func (p *Processor) Start(j *Job) (procTime int) {
+// This method will throw an error if called when there's already a job
+// being processed: that job needs to be Finish()ed first.
+func (p *Processor) Start(j *Job) (procTime int, err error) {
+	if p.CurrentJob != nil {
+		return 0, errors.New("Tried to start job on busy processor; call Finish() first")
+	}
 	p.CurrentJob = j
-	return p.procTimeGenerator(j)
+	return p.procTimeGenerator(j), nil
+}
+
+// Finish empties the current job out of the Processor and returns it.
+//
+// If Finish is called on an idle processor, j will be nil.
+func (p *Processor) Finish() (j *Job) {
+	j = p.CurrentJob
+	p.CurrentJob = nil
+	return j
 }
 
 // NewProcessor creates a new Processor struct.
--- processor_test.go
+++ processor_test.go
@@ -7,8 +7,9 @@ import (
 // Tests the starting of a job
 func TestProcessorStart(t *testing.T) {
 	var proc *Processor
-	var j *Job
+	var j0, j1 *Job
 	var procTime int
+	var err error
 
 	ptg := func(j *Job) int {
 		return 293
@@ -16,11 +17,35 @@ func TestProcessorStart(t *testing.T) {
 
 	proc = NewProcessor()
 	proc.SetProcTimeGenerator(ptg)
-	j = NewJob()
+	j0 = NewJob()
 
-	procTime = proc.Start(j)
+	procTime, err = proc.Start(j0)
 	if procTime != 293 {
 		t.Log("Expected processing time of 293 but got", procTime)
 		t.Fail()
+	}
+	if err != nil {
+		t.Log("Got unexpected error from proc.Start:", err)
+	}
+
+	// Make sure we get an error if we try to start a job while the
+	// processor is busy.
+	j1 = NewJob()
+	procTime, err = proc.Start(j1)
+	if err == nil {
+		t.Log("Expected 'job already in progress error', got no error")
+		t.Fail()
+	}
+
+	// We should still be able to start that new job as long as we
+	// finish the first job:
+	proc.Finish()
+	procTime, err = proc.Start(j1)
+	if procTime != 293 {
+		t.Log("Expected processing time of 293 but got", procTime)
+		t.Fail()
+	}
+	if err != nil {
+		t.Log("Got unexpected error from proc.Start:", err)
 	}
 }
