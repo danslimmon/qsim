ae6aff8f43198c6c135f2452eb945a090015a9df
Dan Slimmon <dan.slimmon@gmail.com>
2015-07-15 11:51:20 +0000

    Inital commit to test Godoc

---
+++ system.go
@@ -0,0 +1,10 @@
+// Package qsim provides a framework for simulating queuing systems.
+//
+// See the readme (https://github.com/danslimmon/qsim/blob/master/README.md)
+// for an overview.
+package qsim
+
+// HerpDerp herps 1 or more derps.
+func HerpDerp() {
+    return
+}
