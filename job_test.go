e259e251c306909ae6939e2a757d4b2082d2873f
Dan Slimmon <dan.slimmon@gmail.com>
2015-07-16 12:11:25 +0000

    Added JobId

---
+++ job_test.go
@@ -0,0 +1,28 @@
+package qsim
+
+import (
+	"testing"
+)
+
+// Tests that JobId is set to a random nonnegative 63-bit integer.
+func TestJobId(t *testing.T) {
+	t.Parallel()
+	var j *Job
+	var jobs map[int64]*Job
+	var i int
+	var ok bool
+
+	jobs = make(map[int64]*Job, 100)
+	for i = 0; i < 100; i++ {
+		j = NewJob(true)
+		if j.JobId < 0 {
+			t.Log("Got negative JobId", j.JobId)
+			t.Fail()
+		}
+		if _, ok = jobs[j.JobId]; ok {
+			t.Log("Got duplicate JobId", j.JobId)
+			t.Fail()
+		}
+		jobs[j.JobId] = j
+	}
+}
