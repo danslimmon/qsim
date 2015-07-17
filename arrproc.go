e79e66571c079d254e6d2941195f240ff8832b27
Dan Slimmon <dan.slimmon@gmail.com>
2015-07-17 13:19:35 +0000

    Added BeforeArrive callback

--- arrproc.go
+++ arrproc.go
@@ -2,9 +2,8 @@ package qsim
 
 // An ArrProc (short for "arrival process") generates new Jobs at some interval.
 type ArrProc interface {
-	// Arrive generates a new job and also returns the interval that will elapse
-	// before the next arrival.
 	Arrive() (j *Job, interval int)
+	BeforeArrive(f func(ap ArrProc))
 }
 
 // ConstantArrProc generates jobs at a constant interval.
@@ -13,11 +12,35 @@ type ArrProc interface {
 type ConstantArrProc struct {
 	// Interval is the interval at which ConstantArrProc will generate Jobs.
 	Interval int
+
+	// Callback lists
+	cbBeforeArrive []func(ap ArrProc)
 }
 
-// Generates a Job and returns the constant value of Interval.
+// Generates a Job and returns the constant value of Interval as the number of
+// ticks that will elapse before the next arrival.
 func (ap *ConstantArrProc) Arrive() (j *Job, interval int) {
+	ap.beforeArrive()
 	j = NewJob()
 	interval = ap.Interval
 	return
+}
+
+// BeforeArrive adds a callback to run immediately before the Arrival Process
+// creates a job. This callback is passed the ArrProc itself.
+func (ab *ConstantArrProc) BeforeArrive(f func(ArrProc)) {
+	ab.cbBeforeArrive = append(ab.cbBeforeArrive, f)
+}
+func (ab *ConstantArrProc) beforeArrive() {
+	for _, cb := range ab.cbBeforeArrive {
+		cb(ab)
+	}
+}
+
+// NewConstantArrProc returns a new ConstantArrProc with the given Interval
+// value.
+func NewConstantArrProc(interval int) (ap *ConstantArrProc) {
+	ap = new(ConstantArrProc)
+	ap.Interval = interval
+	return ap
 }
--- arrproc_test.go
+++ arrproc_test.go
@@ -4,13 +4,14 @@ import (
 	"testing"
 )
 
+// Tests a dead-simple Arrival Process
 func TestConstantArrProc(t *testing.T) {
 	t.Parallel()
 	var ap ArrProc
 	var j *Job
 	var i, time, interval int
 
-	ap = &ConstantArrProc{72}
+	ap = NewConstantArrProc(72)
 	for i = 0; i < 10; i++ {
 		j, interval = ap.Arrive()
 		if j == nil {
@@ -21,6 +22,24 @@ func TestConstantArrProc(t *testing.T) {
 	}
 	if time != 720 {
 		t.Log("Expected", 720, "ticks to elapse from ConstantArrProc arrivals but got", time)
+		t.Fail()
+	}
+}
+
+// Tests the BeforeArrive callback on ConstantArrProc.
+func TestConstantArrProcBeforeArrive(t *testing.T) {
+	t.Parallel()
+	var ap, receivedArrProc ArrProc
+
+	cbBeforeArrive := func(cbArrProc ArrProc) {
+		receivedArrProc = cbArrProc
+	}
+
+	ap = NewConstantArrProc(72)
+	ap.BeforeArrive(cbBeforeArrive)
+	ap.Arrive()
+	if ap != receivedArrProc {
+		t.Log("BeforeArrive ran with wrong ArrProc or didn't run")
 		t.Fail()
 	}
 }
